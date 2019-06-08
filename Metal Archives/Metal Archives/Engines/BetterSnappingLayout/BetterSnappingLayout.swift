b82b8c83c696301d5f601fcc9e3c21353a3b6e6d
Thanh-Nhon NGUYEN <ntnhon.cs@gmail.com>
2019-06-08 16:22:57 +0200

    Fix sticky error BetterSnappingLayout

--- Metal Archives/Metal Archives/Engines/BetterSnappingLayout/BetterSnappingLayout.swift
+++ Metal Archives/Metal Archives/Engines/BetterSnappingLayout/BetterSnappingLayout.swift
@@ -13,21 +13,24 @@ final class BetterSnappingLayout: UICollectionViewFlowLayout {
         guard let collectionView = collectionView else {
             return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
         }
-        
-        var offsetAdjustment = CGFloat.greatestFiniteMagnitude
-        let horizontalOffset = proposedContentOffset.x + collectionView.contentInset.left
+
+        var offsetAdjusment = CGFloat.greatestFiniteMagnitude
+        let horizontalCenter = proposedContentOffset.x + (collectionView.bounds.width / 2)
         
         let targetRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
-        
         let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
-        
         layoutAttributesArray?.forEach({ (layoutAttributes) in
-            let itemOffset = layoutAttributes.frame.origin.x
-            if fabsf(Float(itemOffset - horizontalOffset)) < fabsf(Float(offsetAdjustment)) {
-                offsetAdjustment = itemOffset - horizontalOffset
+            let itemHorizontalCenter = layoutAttributes.center.x
+            
+            if abs(itemHorizontalCenter - horizontalCenter) < abs(offsetAdjusment) {
+                if velocity.x > 0 {
+                    offsetAdjusment = itemHorizontalCenter - horizontalCenter + layoutAttributes.bounds.width
+                } else {
+                    offsetAdjusment = itemHorizontalCenter - horizontalCenter - layoutAttributes.bounds.width
+                }
             }
         })
-        
-        return CGPoint(x: proposedContentOffset.x + offsetAdjustment, y: proposedContentOffset.y)
+
+        return CGPoint(x: proposedContentOffset.x + offsetAdjusment, y: proposedContentOffset.y)
     }
 }
