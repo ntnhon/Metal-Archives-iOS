67ec03cfe9ced9e0473607ff60e7285801aea08e
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2020-05-06 22:52:33 +0200

    Remove unnecessary errors

--- Metal Archives/Metal Archives/Models/Enums/MAError.swift
+++ Metal Archives/Metal Archives/Models/Enums/MAError.swift
@@ -75,10 +75,6 @@ enum MAError: Error, LocalizedError {
     enum ParsingError: LocalizedError {
         /// Bad JSON syntax
         case badJsonSyntax(actualSyntax: Any, expectedSyntax: Any)
-        /// Error while extracting useful informations using regular expresion or string manipulation.
-        case badSyntax(string: String, expectedSyntax: String)
-        /// Error converting extracted informations to a strong type
-        case badType(actualType: Any, expectedType: Any)
         /// HTML syntax is not appropriate
         case badStructure(anyObject: Any)
 
@@ -89,81 +85,10 @@ enum MAError: Error, LocalizedError {
                 Bad JSON syntax: \(actualSyntax.self)
                 Expected syntax: \(expectedSyntax.self)
                 """
-                
-            case .badSyntax(let string, let expectedSyntax):
-                return """
-                Bad syntax: \(string)
-                Expected syntax: \(expectedSyntax.self)
-                """
-                
-            case .badType(let actualType, let expectedType):
-                return """
-                Bad type: \(actualType.self)
-                Expected type: \(expectedType.self)
-                """
-                
+    
             case .badStructure(let anyObject):
                 return "Error parsing \(anyObject.self)"
             }
-        }
-    }
-}
-
-enum MAParsingError: LocalizedError {
-    /// Error while extracting useful informations using regular expresion or string manipulation.
-    case badSyntax(string: String, expectedSyntax: String)
-    /// Error converting extracted informations to a strong type
-    case badType(string: String, expectedType: String)
-    /// HTML syntax is not appropriate
-    case badStructure(objectType: String)
-    /// Invalid request URL
-    case invalidRequestURL(requestURL: String)
-    /// Unknown error
-    case unknown(description: String)
-
-    var localizedDescription: String {
-        switch self {
-        case .badSyntax(let string, let expectedSyntax): return """
-            [MA Parsing Error] Bad syntax for string: \(string).
-            Expected syntax: \(expectedSyntax)
-            """
-        case .badType(let string, let expectedType): return """
-            [MA Parsing Error] Error converting - Bad type for string: \(string)
-            Expected type: \(expectedType)
-            """
-        case .badStructure(let objectType): return "[MAParsingError] Error parsing \(objectType) - Bad HTML structure"
-        case .invalidRequestURL(let requestURL): return "Invalid request URL: \(requestURL)"
-        case .unknown(let description): return "[MAParsingError] Unknown error: \(description)"
-        }
-    }
-}
-
-enum MAFetchingError: LocalizedError {
-    /// Invalid request URL
-    case invalidRequestURL(requestURL: String)
-    case failedToFetch(object: String)
-    
-    var localizedDescription: String {
-        switch self {
-        case .invalidRequestURL(let requestURL): return "Invalid request URL: \(requestURL)"
-        case .failedToFetch(let object): return "[MAFetchingError] Failed to fetch \(object)"
-        }
-    }
-}
-
-enum MANetworkingError: LocalizedError {
-    /// Error creating an URL to make a request
-    case badURL(_ urlString: String)
-    /// Error parsing response from an URL
-    case badResponse(_ response: Any)
-    /// Invalid request URL
-    case invalidRequestURL(requestURL: String)
-    
-    var localizedDescription: String {
-        switch self {
-        case .badURL(let urlString): return "[MA Networking Error] Bad URL: \(urlString)"
-        case .invalidRequestURL(let requestURL): return "Invalid request URL: \(requestURL)"
-        case .badResponse(let response): return "[MA Networking Error] Bad response: \(response)"
         }
     }
 }
