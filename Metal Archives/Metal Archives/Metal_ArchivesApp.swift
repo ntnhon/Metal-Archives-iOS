dc4471f0c5b89ac016b8e7ebf68ddc2c150f49c5
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-05-21 22:12:31 +0200

    Initial commit

---
+++ Metal Archives/Metal Archives/Metal_ArchivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_ArchivesApp.swift
+//  Metal Archives
+//
+//  Created by Thanh-Nhon Nguyen on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Metal_ArchivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
