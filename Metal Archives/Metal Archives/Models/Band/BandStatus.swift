7092f7719fade81e88d2ef4741ab31f1826013d8
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-05-25 09:01:16 +0200

    Add BandStatus

---
+++ Metal Archives/Metal Archives/Models/Band/BandStatus.swift
@@ -0,0 +1,26 @@
+//
+//  BandStatus.swift
+//  Metal Archives
+//
+//  Created by Thanh-Nhon Nguyen on 25/05/2021.
+//
+
+enum BandStatus: String {
+    case active = "Active"
+    case onHold = "On hold"
+    case splitUp = "Split up"
+    case changedName = "Changed name"
+    case unknown = "Unknown"
+    case disputed = "Disputed"
+
+    init(rawValue: String) {
+        switch rawValue.lowercased() {
+        case "active": self = .active
+        case "on hold": self = .onHold
+        case "split-up", "split up": self = .splitUp
+        case "changed name": self = .changedName
+        case "disputed": self = .disputed
+        default: self = .unknown
+        }
+    }
+}
