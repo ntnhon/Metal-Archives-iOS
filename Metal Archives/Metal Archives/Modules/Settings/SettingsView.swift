2ac62cda005d638ee2ba5bdc81b831e5a7e7def0
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-06-19 23:30:14 +0200

    Implement URLButton

--- Metal Archives/Metal Archives/Modules/Settings/SettingsView.swift
+++ Metal Archives/Metal Archives/Modules/Settings/SettingsView.swift
@@ -8,117 +8,134 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @Environment(\.openURL) private var openURL
+    @Environment(\.colorScheme) private var colorScheme
     @State private var showThumbnail = true
     @State private var isHapticOn = false
     let viewModel: SettingsViewModel
 
     var body: some View {
-        Form {
-            Section(header: Text("General")) {
-                Text("Home section order")
+        NavigationView {
+            Form {
+                // General
+                Section(header: Text("General")) {
+                    Text("Home section order")
 
-                Toggle(isOn: $showThumbnail) {
-                    Text("Display thumbnails")
+                    Toggle(isOn: $showThumbnail) {
+                        Text("Display thumbnails")
+                    }
+
+                    HStack {
+                        Text("Discography mode")
+                        Spacer()
+                        Text("Complete")
+                            .font(.body)
+                            .foregroundColor(.gray)
+                    }
+
+                    Toggle(isOn: $isHapticOn) {
+                        Text("Haptic effect")
+                    }
                 }
 
-                HStack {
-                    Text("Discography mode")
-                    Spacer()
-                    Text("Complete")
-                        .font(.body)
-                        .foregroundColor(.gray)
-                }
+                // Bug reports
+                Section(header: Text("Bug reports & feature request"),
+                        footer: bottomFooterView) {
+                    // Twitter
+                    URLButton(urlString: "https://twitter.com/ma_mobile_app") {
+                        HStack {
+                            SettingsIconView(image: Image("Twitter"))
+                            Text("Twitter")
+                        }
+                    }
 
-                Toggle(isOn: $isHapticOn) {
-                    Text("Haptic effect")
+                    // Facebook
+                    URLButton(urlString: "https://www.facebook.com/MetalArchivesIOSApp") {
+                        HStack {
+                            SettingsIconView(image: Image("Facebook"))
+                            Text("Facebook")
+                        }
+                    }
+
+                    // Github
+                    URLButton(urlString: "https://github.com/ntnhon/Metal-Archives-iOS") {
+                        HStack {
+                            if colorScheme == .light {
+                                SettingsIconView(image: Image("Github"))
+                            } else {
+                                SettingsIconView(image: Image("Github")
+                                                    .renderingMode(.template))
+                                    .foregroundColor(.primary)
+                            }
+                            Text("Github")
+                        }
+                    }
+
+                    // Email
+                    URLButton(urlString: "mailto:hi@nguyenthanhnhon.info") {
+                        HStack {
+                            SettingsIconView(image: Image(systemName: "envelope.fill"))
+                            Text("Email")
+                        }
+                    }
                 }
             }
-
-            Section(header: Text("Bug reports & feature request"),
-                    footer: bottomFooterView) {
-                // Twitter
-                HStack {
-                    Image("Twitter")
-                        .resizable()
-                        .modifier(SettingsIcon())
-                    Text("Twitter")
-                }
-
-                // Facebook
-                HStack {
-                    Image("Facebook")
-                        .resizable()
-                        .modifier(SettingsIcon())
-                    Text("Facebook")
-                }
-
-                // Github
-                HStack {
-                    Image("Github")
-                        .resizable()
-                        .modifier(SettingsIcon())
-                    Text("Github")
-                }
-
-                // Email
-                HStack {
-                    Image(systemName: "envelope.fill")
-                        .resizable()
-                        .modifier(SettingsIcon())
-                    Text("Email")
-                }
-            }
+            .navigationTitle("Settings")
         }
-        .navigationTitle("Settings")
     }
 
     private var bottomFooterView: some View {
         VStack {
-            HStack {
-                Text("Made with 🤘 by")
-                Button(action: {
-                        open(urlString: "https://twitter.com/_ntnhon")
-                }, label: {
+            HStack(spacing: 0) {
+                Text("Made with 🤘 by ")
+                URLButton(urlString: "https://twitter.com/_ntnhon") {
                     Text("Nhon Nguyen")
-                })
+                        .fontWeight(.medium)
+                        .foregroundColor(.accentColor)
+                }
             }
 
             Spacer()
 
-            HStack {
-                Text("Icon designed by")
-                Button(action: {
-                        open(urlString: "https://twitter.com/nrthrndarkness")
-                }, label: {
+            HStack(spacing: 0) {
+                Text("App icon designed by ")
+                URLButton(urlString: "https://twitter.com/nrthrndarkness") {
                     Text("Matt Stewart")
-                })
+                        .fontWeight(.medium)
+                        .foregroundColor(.accentColor)
+                }
             }
 
             Spacer()
 
             Text("Version \(viewModel.versionName) (Build \(viewModel.buildNumber))")
+                .fontWeight(.medium)
         }
         .frame(maxWidth: .infinity, alignment: .center)
-    }
-
-    private func open(urlString: String) {
-        guard let url = URL(string: urlString) else { return }
-        openURL(url)
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            SettingsView(viewModel: .init())
-        }
+        SettingsView(viewModel: .init())
+        SettingsView(viewModel: .init())
+            .environment(\.colorScheme, .dark)
     }
 }
 
 private struct SettingsIcon: ViewModifier {
     func body(content: Content) -> some View {
         content
+            .scaledToFit()
+            .frame(width: 18, height: 18)
+    }
+}
+
+private struct SettingsIconView: View {
+    let image: Image
+
+    var body: some View {
+        image
+            .resizable()
             .scaledToFit()
             .frame(width: 18, height: 18)
     }
---
+++ Metal Archives/Metal Archives/Supporting Views/URLButton.swift
@@ -0,0 +1,38 @@
+//
+//  URLButton.swift
+//  Metal Archives
+//
+//  Created by Thanh-Nhon Nguyen on 19/06/2021.
+//
+
+import SwiftUI
+
+struct URLButton<Content: View>: View {
+    @Environment(\.openURL) private var openURL
+    let urlString: String
+    let content: Content
+
+    init(urlString: String, @ViewBuilder content: () -> Content) {
+        self.urlString = urlString
+        self.content = content()
+    }
+
+    var body: some View {
+        Button(action: {
+            if let url = URL(string: urlString) {
+                openURL(url)
+            }
+        }, label: {
+            content
+        })
+        .foregroundColor(.primary)
+    }
+}
+
+struct URLButton_Previews: PreviewProvider {
+    static var previews: some View {
+        URLButton(urlString: "http://example.com") {
+            Text("Example")
+        }
+    }
+}
