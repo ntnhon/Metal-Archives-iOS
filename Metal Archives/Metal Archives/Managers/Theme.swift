63bac226214536b48933d2561e471954839f6fcc
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-06-20 22:08:18 +0200

    Adjust green theme color

--- Metal Archives/Metal Archives/Managers/Theme.swift
+++ Metal Archives/Metal Archives/Managers/Theme.swift
@@ -16,8 +16,10 @@ enum Theme: Int, CaseIterable {
             return Color(.sRGB, red: 114 / 255, green: 77 / 255, blue: 72 / 255, opacity: 1)
         case (.dark, .default):
             return Color(.sRGB, red: 140 / 255, green: 94 / 255, blue: 88 / 255, opacity: 1)
-        case (.light, .green), (.dark, .green):
-            return Color(.sRGB, red: 56 / 255, green: 176 / 255, blue: 0 / 255, opacity: 1)
+        case (.light, .green):
+            return Color(.sRGB, red: 32 / 255, green: 139 / 255, blue: 58 / 255, opacity: 1)
+        case (.dark, .green):
+            return Color(.sRGB, red: 45 / 255, green: 198 / 255, blue: 83 / 255, opacity: 1)
         default: return .blue
         }
     }
@@ -28,8 +30,10 @@ enum Theme: Int, CaseIterable {
             return Color(.sRGB, red: 175 / 255, green: 135 / 255, blue: 120 / 255, opacity: 1)
         case (.dark, .default):
             return Color(.sRGB, red: 211 / 255, green: 171 / 255, blue: 158 / 255, opacity: 1)
-        case (.light, .green), (.dark, .green):
-            return Color(.sRGB, red: 112 / 255, green: 224 / 255, blue: 0 / 255, opacity: 1)
+        case (.light, .green):
+            return Color(.sRGB, red: 74 / 255, green: 214 / 255, blue: 109 / 255, opacity: 1)
+        case (.dark, .green):
+            return Color(.sRGB, red: 183 / 255, green: 239 / 255, blue: 197 / 255, opacity: 1)
         default: return .blue
         }
     }
