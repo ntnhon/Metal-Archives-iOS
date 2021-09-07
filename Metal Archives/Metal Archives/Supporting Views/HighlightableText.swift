ac01ec6e57a7542923d6c1ad88050e90774e5440
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-09-07 22:20:28 +0200

    Implement HighlightableText

---
+++ Metal Archives/Metal Archives/Supporting Views/HighlightableText.swift
@@ -0,0 +1,56 @@
+//
+//  HighlightTextView.swift
+//  Metal Archives
+//
+//  Created by Thanh-Nhon Nguyen on 06/09/2021.
+//
+
+import SwiftUI
+
+private extension HighlightableText {
+    enum Content {
+        case normal(String)
+        case highlight(String)
+    }
+}
+
+struct HighlightableText: View {
+    @EnvironmentObject private var preferences: Preferences
+    private let contents: [Content]
+
+    init(text: String, highlights: [String]) {
+        let separator = UUID().uuidString
+        var text = text
+        highlights.forEach {
+            text = text.replacingOccurrences(of: $0, with: "\(separator)\($0)\(separator)" )
+        }
+        let rawContents = text.components(separatedBy: separator)
+        contents = rawContents.map {
+            if highlights.contains($0) {
+                return Content.highlight($0)
+            }
+            return Content.normal($0)
+        }
+    }
+
+    var body: some View {
+        contents.map { textContent in
+            switch textContent {
+            case .normal(let content):
+                return Text(content)
+            case .highlight(let content):
+                return Text(content)
+                    .foregroundColor(preferences.theme.primaryColor)
+            }
+        }.reduce(Text(""), +)
+    }
+}
+
+struct HighlightTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        // swiftlint:disable:next line_length
+        HighlightableText(text: "(R.I.P. 2001) See also: ex-Control Denied, ex-Mantas, ex-Slaughter, ex-Voodoocult",
+                          highlights: ["Control Denied", "Mantas", "Slaughter", "Voodoocult"])
+            .environmentObject(Preferences())
+    }
+}
