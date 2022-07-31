71d097394ebecf751c035003708f99787a1a9c08
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2022-07-31 10:24:37 +0200

    Show artist detail when tapping on name or instrument texts

--- Metal Archives/Metal Archives/Modules/Band/ArtistInBandView.swift
+++ Metal Archives/Metal Archives/Modules/Band/ArtistInBandView.swift
@@ -26,48 +26,45 @@ struct ArtistInBandView: View {
                 Text(artist.name)
                     .font(.title3.weight(.bold))
                     .foregroundColor(preferences.theme.primaryColor)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .onTapGesture { selectedArtist = artist }
 
                 Text(artist.instruments)
                     .font(.body.weight(.medium))
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .onTapGesture { selectedArtist = artist }
 
                 if let seeAlso = artist.seeAlso {
                     HighlightableText(text: seeAlso,
                                       highlights: artist.bands.map { $0.name })
-                    .frame(
-                        alignment: .leading)
-                }
-            }
-            .overlay {
-                Menu(content: {
-                    Button(action: {
-                        selectedArtist = artist
-                    }, label: {
-                        Label(artist.name, systemImage: "person.circle")
-                    })
-
-                    Divider()
-
-                    ForEach(artist.bands, id: \.thumbnailInfo.id) { band in
-                        Button(action: {
-                            selectedBand = band
+                    .frame(alignment: .leading)
+                    .overlay {
+                        Menu(content: {
+                            ForEach(artist.bands, id: \.thumbnailInfo.id) { band in
+                                Button(action: {
+                                    selectedBand = band
+                                }, label: {
+                                    Text(band.name)
+                                })
+                            }
                         }, label: {
-                            Text(band.name)
+                            Color.clear
                         })
                     }
-
-                    Divider()
-
-                    Button(action: {
-                        showingShareSheet.toggle()
-                    }, label: {
-                        Label("Share", systemImage: "square.and.arrow.up")
-                    })
-                }, label: {
-                    Color.clear
-                })
+                }
             }
+        }
+        .contentShape(Rectangle())
+        .contextMenu {
+            Button(action: {
+                selectedArtist = artist
+            }, label: {
+                Label("View artist detail", systemImage: "person")
+            })
+
+            Button(action: {
+                showingShareSheet.toggle()
+            }, label: {
+                Label("Share", systemImage: "square.and.arrow.up")
+            })
         }
         .sheet(isPresented: $showingShareSheet) {
             if let url = URL(string: artist.thumbnailInfo.urlString) {
