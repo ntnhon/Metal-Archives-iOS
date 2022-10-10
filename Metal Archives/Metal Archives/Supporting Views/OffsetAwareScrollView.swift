727f744d0382b2d5c1a220a04d70b5e05ca41507
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2022-10-10 20:25:55 +0200

    Rearrange files by alphabetical order

--- Metal Archives/Metal Archives/Supporting Views/OffsetAwareScrollView.swift
+++ Metal Archives/Metal Archives/Supporting Views/OffsetAwareScrollView.swift
@@ -7,6 +7,56 @@
 
 import SwiftUI
 
+/*
+struct OffsetAwareVerticalScrollView<V: View>: UIViewRepresentable {
+    let onScroll: (CGPoint) -> Void
+    let content: V
+
+    init(onScroll: @escaping (CGPoint) -> Void,
+         @ViewBuilder content: () -> V) {
+        self.onScroll = onScroll
+        self.content = content()
+    }
+
+    func makeUIView(context: Context) -> UIScrollView {
+        let scrollView = UIScrollView()
+//        scrollView.contentInsetAdjustmentBehavior = .automatic
+//        scrollView.backgroundColor = .clear
+//        scrollView.automaticallyAdjustsScrollIndicatorInsets = false
+        let subview = UIHostingController(rootView: content).view!
+        subview.backgroundColor = .clear
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(subview)
+        scrollView.delegate = context.coordinator
+        let layoutGuide = scrollView.contentLayoutGuide
+        NSLayoutConstraint.activate([
+            subview.topAnchor.constraint(equalTo: layoutGuide.topAnchor),
+            subview.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor),
+            subview.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor),
+            subview.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor),
+            subview.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        return scrollView
+    }
+
+    func updateUIView(_ scrollView: UIScrollView, context: Context) {}
+
+    func makeCoordinator() -> Coordinator { .init(self) }
+
+    class Coordinator: NSObject, UIScrollViewDelegate {
+        let parent: OffsetAwareVerticalScrollView
+
+        init(_ parent: OffsetAwareVerticalScrollView) {
+            self.parent = parent
+        }
+
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            parent.onScroll(scrollView.contentOffset)
+        }
+    }
+}
+*/
+
 // https://betterprogramming.pub/swiftui-calculate-scroll-offset-in-scrollviews-c3b121f0b0dc
 struct OffsetAwareScrollView<T: View>: View {
     let axes: Axis.Set
