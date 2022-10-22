84af4aabade50a1ef073683d97525538b8b1b43a
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2022-10-22 10:08:31 +0200

    Typo fix

--- Metal Archives/Metal Archives/Models/Band/BandByGenre.swift
+++ Metal Archives/Metal Archives/Models/Band/BandByGenre.swift
@@ -1,5 +1,5 @@
 //
-//  BandsByGenre.swift
+//  BandByGenre.swift
 //  Metal Archives
 //
 //  Created by Nhon Nguyen on 22/10/2022.
@@ -7,9 +7,9 @@
 
 import Foundation
 
-typealias BandsByGenre = BandByAlphabet
+typealias BandByGenre = BandByAlphabet
 
-final class BandsByGenrePageManager: PageManager<BandsByGenre> {
+final class BandsByGenrePageManager: PageManager<BandByGenre> {
     init(apiService: APIServiceProtocol, genre: Genre, sortOptions: SortOption) {
         // swiftlint:disable:next line_length
         let configs = PageConfigs(baseUrlString: "https://www.metal-archives.com/browse/ajax-genre/g/\(genre.parameterString)/json/1?sEcho=1&iColumns=4&sColumns=&iDisplayStart=\(kDisplayStartPlaceholder)&iDisplayLength=\(kDisplayLengthPlaceholder)&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&iSortCol_0=\(kSortColumnPlaceholder)&sSortDir_0=\(kSortDirectionPlaceholder)&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=true",
