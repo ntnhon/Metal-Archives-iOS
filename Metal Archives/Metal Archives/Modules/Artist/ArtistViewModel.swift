977dc4cd3d620b3e3b70e09acd825de123742019
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2022-10-28 21:01:00 +0200

    Fetch artist's biography

--- Metal Archives/Metal Archives/Modules/Artist/ArtistViewModel.swift
+++ Metal Archives/Metal Archives/Modules/Artist/ArtistViewModel.swift
@@ -10,6 +10,7 @@ import Combine
 final class ArtistViewModel: ObservableObject {
 //    deinit { print("\(Self.self) of \(urlString) is deallocated") }
     @Published private(set) var artistFetchable: FetchableObject<Artist> = .fetching
+    @Published private(set) var biographyFetchable: FetchableObject<String?> = .fetching
 
     private let apiService: APIServiceProtocol
     private let urlString: String
@@ -28,6 +29,21 @@ final class ArtistViewModel: ObservableObject {
             artistFetchable = .fetched(artist)
         } catch {
             artistFetchable = .error(error)
+        }
+    }
+
+    @MainActor
+    func fetchBiography() async {
+        if case .fetched = biographyFetchable { return }
+        guard let id = urlString.components(separatedBy: "/").last else { return }
+        do {
+            biographyFetchable = .fetching
+            let urlString = "https://www.metal-archives.com/artist/read-more/id/\(id)"
+            let biography = try await apiService.getString(for: urlString, inHtmlFormat: false)
+            print(biography)
+            biographyFetchable = .fetched(biography)
+        } catch {
+            biographyFetchable = .error(error)
         }
     }
 }
