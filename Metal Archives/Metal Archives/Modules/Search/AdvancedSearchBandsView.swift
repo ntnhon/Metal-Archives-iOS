cd526cf0b0d3e0ebd367da9a41c238f10c06f366
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2022-11-20 11:45:19 +0100

    Improve advanced search year picker

--- Metal Archives/Metal Archives/Modules/Search/AdvancedSearchBandsView.swift
+++ Metal Archives/Metal Archives/Modules/Search/AdvancedSearchBandsView.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
+private let kMinYear = 1_960
 private let kThisYear = Calendar.current.component(.year, from: .init())
 
 struct AdvancedSearchBandsView: View {
+    @EnvironmentObject private var preferences: Preferences
     @State private var bandName = ""
     @State private var exactMatch = false
     @State private var genre = ""
     @State private var showCountryList = false
     @StateObject private var countrySet = CountrySet()
-    @State private var fromYear = 1_960
+    @State private var fromYear = kMinYear
     @State private var toYear = kThisYear
     @State private var additionalNote = ""
     @StateObject private var bandStatusSet = BandStatusSet()
@@ -25,7 +27,7 @@ struct AdvancedSearchBandsView: View {
     @State private var indieLabel = false
 
     var body: some View {
-        List {
+        Form {
             Section(header: Text("Band")) {
                 TextField("Band name", text: $bandName)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -43,37 +45,11 @@ struct AdvancedSearchBandsView: View {
                 }
 
                 HStack {
-                    Text("Year of formation from ")
-
-                    Picker(selection: $fromYear,
-                           label: Text(String(format: "%0d", fromYear))
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                            .padding(4)
-                            .background(Color.accentColor)
-                            .clipShape(RoundedRectangle(cornerRadius: 4))) {
-                        ForEach((1_960...kThisYear).reversed(), id: \.self) {
-                            Text(String(format: "%0d", $0))
-                                .tag($0)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-
-                    Text("to")
-
-                    Picker(selection: $toYear,
-                           label: Text(String(format: "%0d", toYear))
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                            .padding(4)
-                            .background(Color.accentColor)
-                            .clipShape(RoundedRectangle(cornerRadius: 4))) {
-                        ForEach((1_960...kThisYear).reversed(), id: \.self) {
-                            Text(String(format: "%0d", $0))
-                                .tag($0)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
+                    Text("Year of formation:")
+                    YearPicker(selectedYear: $fromYear)
+                    Image(systemName: "arrow.right")
+                        .controlSize(.small)
+                    YearPicker(selectedYear: $toYear)
                 }
 
                 NavigationLink(destination: BandStatusListView(bandStatusSet: bandStatusSet)) {
@@ -121,11 +97,48 @@ struct AdvancedSearchBandsView: View {
             }
             .listRowBackground(Color.accentColor)
         }
-        .listStyle(GroupedListStyle())
+        .tint(preferences.theme.primaryColor)
         .navigationBarTitle("Advanced search bands", displayMode: .inline)
     }
 }
 
+private struct YearPicker: View {
+    @EnvironmentObject private var preferences: Preferences
+    @Binding var selectedYear: Int
+
+    var body: some View {
+        Menu(content: {
+            ForEach((kMinYear...kThisYear).reversed(), id: \.self) { year in
+                Button(action: {
+                    selectedYear = year
+                }, label: {
+                    Label(title: {
+                        Text(String(format: "%0d", year))
+                    }, icon: {
+                        if year == selectedYear {
+                            Image(systemName: "checkmark")
+                        }
+                    })
+                })
+            }
+        }, label: {
+            HStack {
+                Text(String(format: "%0d", selectedYear))
+                Image(systemName: "chevron.down")
+                    .imageScale(.small)
+            }
+            .transaction { transaction in
+                transaction.animation = nil
+            }
+            .padding(8)
+            .background(preferences.theme.primaryColor)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+        })
+    }
+}
+
+/*
 struct AdvancedSearchBandsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
@@ -136,3 +149,4 @@ struct AdvancedSearchBandsView_Previews: PreviewProvider {
         .environmentObject(Preferences())
     }
 }
+*/
