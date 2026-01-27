91420362f5e098b83f9fa39583b449b30e1c93e5
ntnhon <ntnhon.cs@gmail.com>
2026-01-27 23:35:17 +0100

    feat: improve homepage section edit

--- Metal Archives/Modules/Settings/HomeSectionOrderView.swift
+++ Metal Archives/Modules/Settings/HomeSectionOrderView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct HomeSectionOrderView: View {
     @EnvironmentObject private var preferences: Preferences
     @Environment(\.editMode) private var editMode
+    @Environment(\.dismiss) private var dismiss
+    @State private var tempOrder: [HomeSection] = []
 
     var body: some View {
         Form {
@@ -17,24 +19,46 @@ struct HomeSectionOrderView: View {
                 ForEach(preferences.homeSectionOrder, id: \.self) {
                     Text($0.description)
                 }
-                .onMove(perform: handleMove)
+                .onMove { src, dest in
+                    tempOrder.move(fromOffsets: src, toOffset: dest)
+                }
             }, footer: {
-                if editMode?.wrappedValue.isEditing == true {
+                if editMode?.wrappedValue == .active {
                     Text("Hold & drag ≡ icon to change order")
                         .animation(.default, value: editMode?.wrappedValue)
                 }
             })
         }
-        .navigationBarTitle("Home section order", displayMode: .inline)
-        .toolbar { EditButton() }
+        .navigationBarTitle("Home section order")
         .onAppear {
-            editMode?.wrappedValue = .active
+            tempOrder = preferences.homeSectionOrder
+        }
+        .toolbar {
+            if editMode?.wrappedValue == .inactive {
+                Button("Edit", systemImage: "square.and.pencil") {
+                    withAnimation {
+                        editMode?.wrappedValue = .active
+                    }
+                }
+            } else if #available(iOS 26, macOS 26, *) {
+                doneButton
+                    .buttonStyle(.glassProminent)
+            } else {
+                doneButton
+            }
         }
     }
+}
 
-    private func handleMove(from source: IndexSet, to destination: Int) {
-        preferences.$homeSectionOrder.wrappedValue.move(fromOffsets: source,
-                                                        toOffset: destination)
+private extension HomeSectionOrderView {
+    var doneButton: some View {
+        Button("Done", systemImage: "checkmark") {
+            withAnimation {
+                editMode?.wrappedValue = .inactive
+                preferences.homeSectionOrder = tempOrder
+                dismiss()
+            }
+        }
     }
 }
 
